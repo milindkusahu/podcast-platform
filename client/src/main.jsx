909191b98b339d3d627b5406ced7007630064e9e
milindkusahu <milindsahu2000@gmail.com>
2025-03-26 12:52:29 +0530

    fix: replace react-helmet-async with react-helmet and simplify main component structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
@@ -10,15 +9,13 @@ import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <ProjectProvider>
-            <App />
-            <Toaster position="bottom-center" />
-          </ProjectProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <ProjectProvider>
+          <App />
+          <Toaster position="bottom-center" />
+        </ProjectProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>
 );
